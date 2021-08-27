fbe03a6d570f5d06633f92779075bf405107661a
haroldo <hcasantos@gmail.com>
2021-08-27 01:07:33 -0300

    Criados e removido atributo de classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,14 @@
 class Pessoa:
+    olhos= 2
+
     def __init__(self,*filhos,nome=None, Idade=35):
         self.idade = Idade
         self.nome = nome
         self.filhos = list(filhos)
 
-
     def cumprimentar(self):
         return f'ola{id(self)}'
+
 
 if __name__ == '__main__':
     haroldo= Pessoa(nome='Haroldo')
@@ -20,8 +22,12 @@ if __name__ == '__main__':
         print(filho.nome)
     aroldo.sobrenome = 'Santos'
     del aroldo.filhos
+    aroldo.olhos=1
+    del aroldo.olhos
     print(aroldo.__dict__)
     print(haroldo.__dict__)
-
-
-
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(aroldo.olhos)
+    print(haroldo.olhos)
+    print(id(Pessoa.olhos), id(aroldo), id(haroldo))
